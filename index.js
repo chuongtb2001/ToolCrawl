6a80c3b3f38aa1b2d0b31b5584e98f7e91717cbe
Your Name <you@example.com>
2023-05-15 07:33:21 +0700

    test change proxy

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const Papa = require('papaparse');
+const proxyChain = require('proxy-chain');
+
+// const proxies = ["203.205.34.58:5578","45.118.139.196:80","118.70.126.245:5678"]
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const oldProxyUrl = 'http://ABUxEp:sUWc0m@181.177.87.231:9469';
+  const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
+
+  // Prints something like "http://127.0.0.1:12345"
+  console.log(newProxyUrl);
+
+  const browser = await puppeteer.launch({
+      args: [`--proxy-server=${newProxyUrl}`],
+  });
+
   const page = await browser.newPage();
-  let pageNumber = 0
+  // const x = await page.goto(`https://www.fanatics.com/nba/los-angeles-lakers/men/o-2447+t-69584146+ga-56+z-820-817603699`,{timeout: 300000});
+  // console.log(x.status())
+
+  let pageNumber = 0  
   // let amountPerPage = 1
   let productsJson = []
-  //get products in page < 1
-  while (pageNumber < 1) {
+  // get products in page < 1
+  while (pageNumber < 100) {
     pageNumber += 1
-    console.log("--------------------------------")
     const productInfo = {
       productName: "",
       productPrice: "",
       productPriceSale: "",
       productImgs: []
     }
-    const pageList = await page.goto(`https://www.fanatics.com/nba/los-angeles-lakers/men/o-2447+t-69584146+ga-56+z-820-817603699?pageSize=72&pageNumber=${pageNumber}&sortOption=TopSellers`);
+    const pageList = await page.goto(`https://www.fanatics.com/nba/los-angeles-lakers/men/o-2447+t-69584146+ga-56+z-820-817603699?pageSize=72&pageNumber=${pageNumber}&sortOption=TopSellers`, { timeout: 300000 });
+    console.log(pageList.status())
     if (pageList.status() !== 200) {
       continue
     }
@@ -28,7 +43,7 @@ const Papa = require('papaparse');
     for (const productHref of productHrefList) {
       //get page
       try {
-        const pageProduct = await page.goto(productHref);
+        const pageProduct = await page.goto(productHref, { timeout: 300000 });
         if (pageProduct.status() !== 200) {
           continue
         }
