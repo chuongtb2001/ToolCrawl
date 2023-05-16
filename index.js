e9c0dc9ae07022fc321c8a3588487f76325a6eea
chuongtb <chuongbinhtran2001@gmail.com>
2023-05-16 22:52:36 +0700

    test crawl

--- index.js
+++ index.js
@@ -1,88 +1,81 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const Papa = require('papaparse');
-const proxyChain = require('proxy-chain');
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+const proxyChain = require('proxy-chain')
+const useProxy = require('puppeteer-page-proxy')
 
-(async () => {
-  const proxies = [
-    "http://171.251.245.38:10003",
-    "http://27.72.244.228:8080",
-    "http://171.160.209.113:19132",
-    "http://G98SWs:Un98Y4@45.145.57.235:12790",
-    "http://ABUxEp:sUWc0m@181.177.87.231:9469",
-  ];
+const proxies = [
+  "http://kYEYnV:3STBp4@181.177.103.220:9580",
+  "http://kYEYnV:3STBp4@131.108.17.29:9202",
+  "http://ABUxEp:sUWc0m@181.177.87.231:9469", //can access
+]
 
-  let proxyUrlIndex = 0
+let countReq = 0;
+let proxyUrlIndex = 0;
 
-  const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--disable-web-security"],
-  });
+async function changeProxy(page){
+  page.removeAllListeners("request")
 
-  const page = await browser.newPage();
-
-  await page.setRequestInterception(true);
+  await page.setRequestInterception(true)
   page.on("request", async (request) => {
-    const oldProxyUrl = proxies[proxyUrlIndex];
-    const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
-    request.continue({
-      proxy: newProxyUrl,
-    });
-  });
-  let pageNumber = 1;
-  let productsJson = [];
-  // get products in page < 1
+    if (request.resourceType() !== "document") {
+      request.abort()
+    } else {
+      countReq += 1
+      if(countReq > 20) {
+        proxyUrlIndex =
+          proxyUrlIndex >= proxies.length - 1 ? 0 : proxyUrlIndex + 1
+        //reset count request
+        countReq = 0
+        console.log("Change proxy")
+      }
+      const oldProxyUrl = proxies[proxyUrlIndex]
+      console.log(oldProxyUrl)
+
+      const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl)
+      await useProxy(request, newProxyUrl)
+    }
+  })
+}
+(async () => {
+  const browser = await puppeteer.launch({})
+  const page = await browser.newPage()
+  await changeProxy(page, proxies[proxyUrlIndex])
+
+  let pageNumber = 1  
+  // get products
   while (pageNumber < 100) {
     const productInfo = {
+      productUrl: "",
       productName: "",
       productPrice: "",
       productPriceSale: "",
       productImgs: [],
-    };
-    let mainPage
-    while (true) {
-      try {
-        mainPage = await page.goto(
-          `https://www.fanatics.com/nba/los-angeles-lakers/men/o-2447+t-69584146+ga-56+z-820-817603699?pageSize=72&pageNumber=${pageNumber}&sortOption=TopSellers`,
-          { timeout: 300000 }
-        );
-        console.log(mainPage.status());
-        break
-      } catch (error) {
-        console.log(error);
-        //handle
-        page.removeAllListeners("request");
-        proxyUrlIndex = proxyUrlIndex >= proxies.length - 1? 0 : proxyUrlIndex + 1
-        await page.setRequestInterception(true);
-        page.on("request", async (request) => {
-          const oldProxyUrl = proxies[proxyUrlIndex];
-          const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
-          request.continue({
-            proxy: newProxyUrl,
-          });
-        });
-      }
     }
-    const title = await page.title()
-    console.log(title);
+    const mainPage = await page.goto(
+        `https://www.fanatics.com/nba/los-angeles-lakers/men/o-2447+t-69584146+ga-56+z-820-817603699?pageSize=72&pageNumber=${pageNumber}&sortOption=TopSellers`,
+        { timeout: 100000 }
+      )
+      // const data = await useProxy.lookup(page)
+      // console.log(data.ip)
+      // change ip
     const productHrefList = await page.$$eval(
       ".product-image-container a",
       (elements) => {
-        return elements.map((el) => el.href);
+        return elements.map((el) => el.href)
       }
-    );
-        console.log(productHrefList.length);
-
+    )
     for (const productHref of productHrefList) {
+      //
+      productInfo.productUrl = productHref
       //get page
       try {
-        const pageProduct = await page.goto(productHref, { timeout: 300000 });
+        const pageProduct = await page.goto(productHref, { timeout: 300000 })
         if (pageProduct.status() !== 200) {
-          continue;
+          continue
         }
       } catch (error) {
-        console.log(error);
-        continue;
+        console.log(error)
+        continue
       }
       //get information about products
 
@@ -91,47 +84,44 @@ const proxyChain = require('proxy-chain');
         productInfo.productName = await page.$eval(
           ".product-title-container h1",
           (el) => el.textContent
-        );
+        )
       } catch (error) {
-        productInfo.productName = "";
+        productInfo.productName = ""
       }
       //get product origin price
       try {
         productInfo.productPrice = await page.$eval(
           ".pdp-price .price:not(.primary) .sr-only",
           (el) => el.textContent
-        );
+        )
       } catch (error) {
-        productInfo.productPrice = "";
+        productInfo.productPrice = ""
       }
       //get product sale price
       try {
         productInfo.productPriceSale = await page.$eval(
           ".pdp-price .price.primary .sr-only",
           (el) => el.textContent
-        );
+        )
       } catch (error) {
-        productInfo.productPriceSale = "";
+        productInfo.productPriceSale = ""
       }
       //get product images
       try {
         productInfo.productImgs = await page.$$eval(
           ".thumbnail-images img",
           (elements) => elements.map((el) => el.src)
-        );
+        )
       } catch (error) {
-        productInfo.productImgs = [];
+        productInfo.productImgs = []
       }
-
-      productsJson = [productInfo];
-      console.log("Crawl done: " + productInfo.productName);
       //import new product to csv
-      const csv = Papa.unparse(productsJson);
-      fs.appendFile("output.csv", csv, (err) => {
-        if (err) throw err;
-        console.log(`${productInfo.productName} is saved successfully`);
-      });
+      fs.appendFile("output.csv", `${productInfo.productUrl},${productInfo.productName},${productInfo.productPrice},${productInfo.productPriceSale},"${productInfo.productImgs.toString()}"\n`, (err) => {
+        if (err) throw err
+        console.log(`${productInfo.productName} is saved successfully`)
+        console.log("---")
+      })
     }
   }
-  await browser.close();
-})();
+  await browser.close()
+})()
